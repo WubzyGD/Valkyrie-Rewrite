d72b6e6eaad16b2fa0fd9ccdd60f68f484f24c07
WubzyGD <wubzygd@gmail.com>
2021-01-24 19:43:10 -0700

    add serverinfo

--- events/message.js
+++ events/message.js
@@ -4,7 +4,7 @@ const wait = require('../util/wait');
 
 module.exports = async (client, message) => {
     if (message.author.bot) {return undefined;}
-	if (message.channel.type == 'dm') /*{var dmch = true;} else {var dmch = false};*/ {return undefined;}
+	if (message.channel.type === 'dm') /*{let dmch = true;} else {let dmch = false};*/ {return undefined;}
 	if (message.channel.type !== 'text' && message.channel.type !== 'dm') {return undefined;}
 
 	//if (message.channel.type == "text") {if (settings[message.guild.id]) {prefix = settings[message.guild.id].prefix;};};
@@ -13,14 +13,14 @@ module.exports = async (client, message) => {
 	
     const prefix = 'v..';
 
-	var msg = message.content.toLowerCase();
-	var mention = message.mentions.users.first();
-    var args = msg.startsWith(prefix) 
+	let msg = message.content.toLowerCase();
+	let mention = message.mentions.users.first();
+    let args = msg.startsWith(prefix)
         ? message.content.slice(prefix.length).trim().split(/\s+/g) 
         : msg.startsWith('<@!') 
             ? message.content.slice(4 + client.user.id.length).trim().split(/\s+/g)
             : message.content.slice(3 + client.user.id.length).trim().split(/\s+/g);
-	var cmd = args.shift().toLowerCase().trim();
+	let cmd = args.shift().toLowerCase().trim();
 
     try {
         if (msg.startsWith(prefix) || msg.startsWith(`<@${client.user.id}>`) || msg.startsWith(`<@!${client.user.id}>`)) {
@@ -30,10 +30,11 @@ module.exports = async (client, message) => {
             await wait(800);
             message.channel.stopTyping();
             require('../util/oncommand')(message, msg, args, cmd, prefix, mention, client);
+            if (command.meta.guildOnly && !message.guild) {return message.channel.send("That command can only be used in a server!");}
             command.execute(message, msg, args, cmd, prefix, mention, client);
         }
     } catch (e) {
-        var date = new Date; date = date.toString().slice(date.toString().search(":") - 2, date.toString().search(":") + 6);
+        let date = new Date; date = date.toString().slice(date.toString().search(":") - 2, date.toString().search(":") + 6);
         console.error(`\n${chalk.red('[ERROR]')} >> ${chalk.yellow(`At [${date}] | In ${message.guild.name}\n`)}`, e);
     }
 };
